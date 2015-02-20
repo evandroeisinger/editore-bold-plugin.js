a72bf7b66d0cbe04bbc9b8956928efa80ce8bdf0
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-20 16:21:58 -0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# editore.js bold plugin
+
+#### basic usage
+It's easy to use! Load [editor.js](https://github.com/evandroeisinger/editore.js) into your application, instantiate it and register the new edition plugin.
+
+```javascript
+var editor = new Editor(document.getElementById('editor'));
+// then register!
+editor.register('edition', window.BoldEditionPlugin);
+```
+
+---
+#### support
+- chrome: ?
+- firefox: ?
+- safari: ?
+- internet explore: ?
+
+
+---
+#### contribute
+Everyone can contribute! Finding bugs, creating issues, improving editor it self or creating components.
+Every contribution will be welcomed! :santa: 
+
+**Fork it** -> **Branch it** -> **Test it** -> **Push it** -> **Pull Request it** :gem:  
---
+++ src/bold-edition-plugin.js
@@ -0,0 +1,33 @@
+(function(global, plugin) {
+  'use strict';
+
+  if (typeof define === 'function' && define.amd)
+    define('bold-edition-plugin', plugin);
+  else if (typeof exports !== 'undefined')
+    exports.BoldEditionPlugin = plugin();
+  else
+    global.BoldEditionPlugin = plugin();
+}(window, function() {
+  'use strict';
+
+  function BoldEditionPlugin() {
+    var self = this;
+    // set plugin elements/props
+    self.button = document.createElement('button');
+    self.button.innerText = 'bold';
+    self.name = 'BoldEditionPlugin';
+    self.tag = 'b';
+  }
+
+  BoldEditionPlugin.prototype = {
+    action: function(field, e) {
+      e.preventDefault();
+      document.execCommand('bold', false, null);
+    },
+
+    destroy: function() {}
+  };
+
+  return BoldEditionPlugin;
+}));
+    
